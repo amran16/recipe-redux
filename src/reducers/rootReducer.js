aedac8604a0ddb1d696943eb4bfc05b127aa348b
amran16 <maryamg99@gmail.com>
2018-08-12 19:57:12 -0700

    Updates

--- src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers} from 'redux';
-import recipeRecuder from './reducer_recipe';
-
-const rootReducer = combineReducers({
-  recipes: recipeRecuder
-});
-
-export default rootReducer;
